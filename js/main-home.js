e68ec03ddce95ba6e7b94e089764b3e7388466e5
kamidem <77845156+kamidem@users.noreply.github.com>
2021-03-25 11:34:44 +0000

    js data added

--- js/main-home.js
+++ js/main-home.js
@@ -1,22 +1,9 @@
-// //import sum from './components/sum.js';
-// //import {multiply as daugyba} from './components/multiply.js';
-// //import {sum, minus, multiply, divide} from './components/matematika.js';
-// import {matematika} from './components/matematika.js';
-
-// const rez1 = matematika.sudeti(8, 5)
-// const rez2 = matematika.padalinti(8, 5);
-
-// console.log(rez1);
-// console.log(rez2);
-
 //IMPORT
 //hero
-import {clock} from '';
 //main
 //footer
 
 // FUNKCIJU PANAUDOJIMAS
 //hero
-clock();
 //main
 //footer
